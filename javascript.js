5a1f63cd932a60e6c3da44fd1f9a0574274d6e66
cassy raina <cassyraina@gmail.com>
2021-07-19 11:41:16 +0200

    console

---
+++ javascript.js
@@ -0,0 +1,5 @@
+
+  
+  console.log("y"); // Affiche "1" dans la console
+  console.log("z"); // Lève une exception ReferenceError:
+                  // z n'est pas définie en dehors de x
---
+++ javascript1.js
@@ -0,0 +1,7 @@
+var x = new Boolean("false");
+if (x)
+if ( confirm( "Message à afficher" )) {
+    // Code à éxécuter si le l'utilisateur clique sur "OK"
+} else {
+    // Code à éxécuter si l'utilisateur clique sur "Annuler" 
+}
